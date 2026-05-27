b8cce6a6aa44243a8d9f1ed8865cfe66d043af41
Eric Matteucci <eric.matteucci@ravelin.com>
2026-05-27 17:22:31 +0100

    add core functions and unit tests

---
+++ src/bundle/core.ts
@@ -0,0 +1,9 @@
+import { Core, type CoreConfig } from '../core';
+
+export default class PersistentDeviceId {
+  public core: Core;
+
+  public constructor(cfg: CoreConfig = {}) {
+    this.core = new Core(cfg);
+  }
+}
